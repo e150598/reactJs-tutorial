e08c4bb9a7a3b050632be374993c9ecf95a914f7
Elvin Hatamov <101150598@georgebrown.ca>
2021-02-16 22:09:25 -0500

    first commit

---
+++ tutorial/src/books.js
@@ -0,0 +1,21 @@
+export const data = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/51eAMQG-GkL.jpg',
+    title: 'I love you to the Moon and Back',
+    author: 'Amazon',
+  },
+
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/61AyQ-tUA8L._AC_UY218_.jpg',
+    title: 'Hugs of Love',
+    author: 'Christopher Greyson',
+  },
+  {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/711kBmsWLnL._AC_UL320_.jpg',
+    title: 'The Fun and easy Memmory',
+    author: 'J. D. Kinnest',
+  },
+]
